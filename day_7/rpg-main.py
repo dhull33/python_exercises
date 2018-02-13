f841e842f7d9e4c04e6c0e15883baa1c2c2f76ed
David Hull <david.hull1@utexas.edu>
2018-02-13 15:24:43 -0600

    Updated main

--- day_7/rpg-main.py
+++ day_7/rpg-main.py
@@ -14,20 +14,22 @@ goblin.alive()
 hero.alive()
 hero.alive
 
-while hero.alive() and zombie.alive():
+while hero.alive() and goblin.alive():
     print('You have {} health and {} power.'.format(hero.health, hero.power))
     print('The zombie has {} health and {} power.'.format(zombie.health, zombie.power))
+    print('The goblin has {} health and {} power.'.format(goblin.health, goblin.power))
     print()
     print('What would you like to do?')
-    print('1: Fight that motherfucker')
-    print('2: Stand there like a brain dead turd')
-    print('3. Bitch out')
+    print('1: Fight the zombie')
+    print('2: Fight the goblin')
+    print('3: Observe the creatures')
+    print('4: Flee')
     print('>', end = ' ')
     
     raw_input = int(input())
     
     if raw_input == 1:
-        # Hero fights goblin
+        # Hero fights zombie
         print("Prepare to meet your doom zombie!")
         
         hero.attack(zombie)
@@ -35,18 +37,52 @@ while hero.alive() and zombie.alive():
         print("You did {} damage to the zombie! Fuck yeah!".format(hero.power))
         zombie.never_die
         print("")
+        print("Oh no! The zombie has gained 6 health points! WTF?!?!")
         
-        if not zombie.alive():
+        if zombie.alive():
+            print("I AM COMING FOR YOU HERO!!!!! --ZomZom")
+            zombie.attack(hero)
+            print('')
+            print('The zombie has taken {} health from you hero.'.format(zombie.power))
+            zombie.never_die()
+            print("What is dead may never die...")
+            print()
+        
+        if hero.alive():
+            pass
+        else:
+            print("You are a disgrace.")
+        continue
+        
+    elif raw_input == 2:
+        #Hero fights goblin
+        print("Prepare to meet your doom goblin!")
+        hero.attack(goblin)
+        print("")
+        print("You did {} damage to the goblin! Fuck yeah!".format(hero.power))
+        
+        if goblin.alive():
+            print("I AM COMING FOR YOU HERO!!!!! --GobGob")
+            goblin.attack(hero)
+            print('')
+            print('The goblin has taken {} health from you hero.'.format(goblin.power))
+            print('')
+        else:
             print("You have defeated the goblin!!\nThe realm is finally at peace thanks to your bravery.")
+            
+        if hero.alive():
+            pass
+        else:
+            print("You are a disgrace.")
         
           
         
             
-    elif raw_input == 2:
-        pass
-    
-    
     elif raw_input == 3:
+        print("They are interesting creatures indeed...")
+    
+    
+    elif raw_input == 4:
         print("You have brought shame upon your family.")
         break
     
@@ -55,14 +91,3 @@ while hero.alive() and zombie.alive():
         break
     
     
-    if zombie.alive():
-        print("I AM COMING FOR YOU HERO!!!!! --ZomZom")
-        zombie.attack(hero)
-        print('')
-        print('The zombie has taken {} health from you hero.'.format(zombie.power))
-        zombie.never_die()
-        
-        if hero.alive():
-            pass
-        else:
-            print("You are a disgrace.")
